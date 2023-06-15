d3f158c9f3f4bf15dafffb2e632b24407a35783e
stitch-606 <stitch2425100@gmail.com>
2023-06-15 06:23:55 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+/*
+print the asked text 
+*/
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
