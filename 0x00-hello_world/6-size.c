b5f13c93466876f105053c74c47277073173ccbc
stitch-606 <stitch2425100@gmail.com>
2023-06-15 07:21:55 -0700

    6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main fun
+ *printf : print sizes
+ * return : allways 0
+ */
+int main(void)
+{
+printf(sizeof(char))
+printf(sizeof(unsigned char))
+printf(sizeof(short))
+printf(sizeof(unsigned short))
+printf(sizeof(int))
+printf(sizeof(unsigned int))
+printf(sizeof(long))
+printf(sizeof(unsigned long))
+return (0);
+}
