4a075e3a8f2a3d9c55ae93c99506efdd341b5d0f
stitch-606 <stitch2425100@gmail.com>
2023-06-15 07:31:40 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
-/**
- * main - main fun
- *printf : print sizes
- * return : allways 0
- */
+
 int main(void)
 {
-printf(sizeof(char));
-printf(sizeof(unsigned char));
-printf(sizeof(short));
-printf(sizeof(unsigned short));
-printf(sizeof(int));
-printf(sizeof(unsigned int));
-printf(sizeof(long));
-printf(sizeof(unsigned long));
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 return (0);
 }
+
