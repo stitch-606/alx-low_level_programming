8fdfe832d01c27df60cc6671570db0e7bd2c7daa
stitch-606 <stitch2425100@gmail.com>
2023-06-15 07:26:53 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,13 +6,13 @@
  */
 int main(void)
 {
-printf(sizeof(char))
-printf(sizeof(unsigned char))
-printf(sizeof(short))
-printf(sizeof(unsigned short))
-printf(sizeof(int))
-printf(sizeof(unsigned int))
-printf(sizeof(long))
-printf(sizeof(unsigned long))
+printf(sizeof(char));
+printf(sizeof(unsigned char));
+printf(sizeof(short));
+printf(sizeof(unsigned short));
+printf(sizeof(int));
+printf(sizeof(unsigned int));
+printf(sizeof(long));
+printf(sizeof(unsigned long));
 return (0);
 }
