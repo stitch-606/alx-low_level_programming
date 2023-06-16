1ee9c9aac1c576e130158eb67d1dbbeafba0d81e
stitch-606 <stitch2425100@gmail.com>
2023-06-15 19:59:19 -0700

    8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <unistd.h>
+
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: 1 (to indicate an error)
  */
 int main(void)
 {
-write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
- return (1);
+char *message = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
+int length = 59;
+write(STDERR_FILENO, message, length);
+return (1);
 }
