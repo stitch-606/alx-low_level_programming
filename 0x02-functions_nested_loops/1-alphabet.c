1683643ec679cfac1bc529023180693efd30c921
stitch-606 <stitch2425100@gmail.com>
2023-06-20 07:45:32 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+char lett;
+for (lett = 'a', lett <= 'z', lett++)
+{
+_putchar(lett);
+}
+_putchar('\n')
+return (0);
+}
