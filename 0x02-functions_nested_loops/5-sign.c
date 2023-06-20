0c2d65d06e1e613f89181cc6be6cd405ba19f42e
stitch-606 <stitch2425100@gmail.com>
2023-06-20 11:12:39 -0700

    5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,27 +11,18 @@ int print_sign(int n)
 if (n > 0)
 {
 _putchar('+');
-_putchar(',');
-_putchar(' ');
-_putchar(n + '0');
 _putchar('\n');
 return (1);
 }
 else if (n == 0)
 {
 _putchar('0');
-_putchar(',');
-_putchar(' ');
-_putchar(n + '0');
 _putchar('\n');
 return (0);
 }
 else
 {
 _putchar('-');
-_putchar(',');
-_putchar(' ');
-_putchar(-n + '0');
 _putchar('\n');
 return (-1);
 }
